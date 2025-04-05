a5a2ea91d348021c435187800ed6e8d7cc037fe9
Hudson Felipe <hudson0211@gmail.com>
2025-04-05 18:55:54 -0300

    feat: update React and React Router versions, implement routing in App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes as Router } from 'react-router-dom';
 import Home from './pages/Home';
+import LauchOverview from './pages/LauchOverview';
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Router>
+        <Route path="/" element={<Home />} />
+        <Route path="/lauch-overview" element={<LauchOverview />} />
+      </Router>
+    </BrowserRouter>
   );
 }
 
